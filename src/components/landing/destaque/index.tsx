dea90a413a585a904b7c7f9d1b334f0187da1f10
dayannealcantara <dayannealcantara14@gmail.com>
2023-05-11 14:35:09 -0300

    feat: developing highlight container component

--- src/components/landing/destaque/index.tsx
+++ src/components/landing/destaque/index.tsx
@@ -1,5 +1,21 @@
+import CommonArea from "../comum/Area";
+import ImagemResponsiva from "../comum/ImagemResponsiva";
+import Slogan from "./Slogan";
+import principal from "../../../../public/principal.jpg"
+
 export default function Emphasis() {
   return (
-      <div>Destaques</div>
+     <CommonArea id="inicio" className="pt-20">
+      <div className={`
+        flex items-center justify-around
+        h-[500px]
+     `}>
+        <Slogan/>  
+        <ImagemResponsiva
+          imagem={principal}
+          className="rotate-3 hidden md:inline"
+        />      
+      </div>
+     </CommonArea>
   )
 }
