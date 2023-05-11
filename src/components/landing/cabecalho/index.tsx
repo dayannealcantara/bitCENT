860cfe9f1c616af76bdcaaabf2a0377e02dfdb69
dayannealcantara <dayannealcantara14@gmail.com>
2023-05-11 14:33:12 -0300

    feat:developing header component

--- src/components/landing/cabecalho/index.tsx
+++ src/components/landing/cabecalho/index.tsx
@@ -1,7 +1,17 @@
+import CommonArea from "../comum/Area";
 import Logo from "../comum/Logo";
+import Menu from "./Menu";
 
 export default function Cabecalho() {
-  return (
-      <Logo/>
+  return (    
+    <CommonArea className="bg-black fixed z-50">
+      <div className="flex items-center justify-between h-20">
+        <Logo /> 
+        <Menu/>     
+      </div>
+    </CommonArea>
   )
 }
+
+
+ 
