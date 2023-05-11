6b709c2809fb180830c043d6f9836e1cfc8b2fd1
dayannealcantara <dayannealcantara14@gmail.com>
2023-05-11 16:21:03 -0300

    feat: developing fake user component

---
+++ src/data/constants/usuario.ts
@@ -0,0 +1,8 @@
+import Usuario from "@/logic/core/usuario/Usuario";
+
+export default {
+    id: "1",
+    nome: 'Dayanne Alcantara',
+    email: 'dayanne@xmail.com',
+    imagemUrl: null,
+} as Usuario
