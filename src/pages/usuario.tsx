9a47cd5a93d44037feea3f09a96962e96227e7ff
dayannealcantara <dayannealcantara14@gmail.com>
2023-05-11 17:02:50 -0300

    feat:starting user page

---
+++ src/pages/usuario.tsx
@@ -0,0 +1,13 @@
+
+import Pagina from "@/components/template/Pagina";
+import Header from "@/components/template/Header";
+
+export default function CadastroUsuario() {
+
+  return (
+    <Pagina>
+      <Header/>    
+      <div>Usuário</div>        
+    </Pagina>
+  )
+}
