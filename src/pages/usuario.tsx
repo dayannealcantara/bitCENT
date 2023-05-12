b6fc2f48265a68e1e5f07c0176efef6c769072d7
dayannealcantara <dayannealcantara14@gmail.com>
2023-05-11 21:35:48 -0300

    feat: finishing user page

--- src/pages/usuario.tsx
+++ src/pages/usuario.tsx
@@ -1,13 +1,25 @@
 
 import Pagina from "@/components/template/Pagina";
 import Header from "@/components/template/Header";
+import TituloPagina from "@/components/template/TituloPagina";
+import { IconForms } from "@tabler/icons-react";
+import usuario from "@/data/constants/usuario";
+import Conteudo from "@/components/template/Conteudo";
+import Formularios from "@/components/usuario/Formularios";
 
 export default function CadastroUsuario() {
 
   return (
     <Pagina>
-      <Header/>    
-      <div>Usuário</div>        
+      <Header/> 
+        <Conteudo>
+          <TituloPagina
+          icone={<IconForms />}
+          principal="Dados Cadastrais"
+          secundario={`Informações de ${usuario.email}`}
+          />  
+          <Formularios/>
+        </Conteudo>          
     </Pagina>
   )
 }
