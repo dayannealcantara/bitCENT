9d0d816c4ddbd0eb1b17f5c3bd0409f80a9aa082
dayannealcantara <dayannealcantara14@gmail.com>
2023-05-12 14:26:22 -0300

    feat:removing mock and passing transaction control methods with useTransacao hook

--- src/components/financas/index.tsx
+++ src/components/financas/index.tsx
@@ -1,35 +1,19 @@
-
 import Transacao, { transacaoVazia } from "@/logic/core/financas/Transacao";
 import Conteudo from "../template/Conteudo";
 import Header from "../template/Header";
 import Pagina from "../template/Pagina";
 import Lista from "./Lista";
-import transacoesFalsas from "@/data/constants/transacoes";
-import { useState } from "react";
 import Formulario from "./Formularios";
 import NaoEncontrado from "../template/NaoEncontrado";
-import Id from "@/logic/core/comum/Id";
 import { Button } from "@mantine/core";
 import { IconPlus } from "@tabler/icons-react";
+import useTransacao from "@/data/hooks/useTransacao";
+
 
 export default function Financas() {
-  const [transacoes, setTransacoes] = useState<Transacao[]>(transacoesFalsas)
-  const [transacao, setTransacao] = useState<Transacao | null>(null)
-
-  function salvar (transacao:Transacao) {
-    const outrasTransacoes = transacoes.filter(t => t.id !== transacao.id)    
-    setTransacoes([...outrasTransacoes, {
-      ...transacao,
-      id: transacao.id ?? Id.novo()
-    }])
-    setTransacao(null)
-  }
-
-  function excluir(transacao:Transacao){
-    const outrasTransacoes = transacoes.filter(t => t.id !== transacao.id)
-    setTransacoes(outrasTransacoes)
-    setTransacao(null)
-  }
+  const { data, alterarData, alterarExibicao, tipoExibicao,
+  transacoes, transacao, selecionar, salvar, excluir
+} = useTransacao()
 
   return(
     <Pagina>
@@ -37,18 +21,18 @@ export default function Financas() {
         <Button
           className="bg-blue-500"
           leftIcon={<IconPlus />}
-          onClick={() => setTransacao(transacaoVazia)}
+          onClick={() => selecionar(transacaoVazia)}
           >Nova transação</Button>
       <Conteudo className="gap-5">
         {transacao ? (
           <Formulario
             transacao={transacao}
-            cancelar={() => setTransacao(null)}
+            cancelar={() => selecionar(null)}
             salvar={salvar}
             excluir={excluir}
           />
         ) : transacoes.length > 0 ? (
-          <Lista transacoes={transacoes} selecionarTransacao={setTransacao}/>
+          <Lista transacoes={transacoes} selecionarTransacao={selecionar}/>
         ) : (
           <NaoEncontrado>
             Nenhuma transação encontrada
