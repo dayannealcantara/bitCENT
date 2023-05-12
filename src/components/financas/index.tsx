4b2cd0125671fa0a23b802fa789885946f0db2d0
dayannealcantara <dayannealcantara14@gmail.com>
2023-05-12 15:39:38 -0300

    feat:rearranging component, importing component from mesaYear and grid

--- src/components/financas/index.tsx
+++ src/components/financas/index.tsx
@@ -1,13 +1,15 @@
-import Transacao, { transacaoVazia } from "@/logic/core/financas/Transacao";
+import  { transacaoVazia } from "@/logic/core/financas/Transacao";
 import Conteudo from "../template/Conteudo";
 import Header from "../template/Header";
 import Pagina from "../template/Pagina";
 import Lista from "./Lista";
 import Formulario from "./Formularios";
 import NaoEncontrado from "../template/NaoEncontrado";
-import { Button } from "@mantine/core";
-import { IconPlus } from "@tabler/icons-react";
-import useTransacao from "@/data/hooks/useTransacao";
+import { Button, SegmentedControl } from "@mantine/core";
+import { IconLayoutGrid, IconList, IconPlus } from "@tabler/icons-react";
+import useTransacao, { TipoExibicao } from "@/data/hooks/useTransacao";
+import CampoMesAno from "../template/CampoMesAno";
+import Grade from "./Grade";
 
 
 export default function Financas() {
@@ -15,15 +17,43 @@ export default function Financas() {
   transacoes, transacao, selecionar, salvar, excluir
 } = useTransacao()
 
+function renderizarControles() {
+  return (
+    <div className="flex justify-between">
+      <CampoMesAno
+        data={data}
+        dataMudou={alterarData}
+      />
+        <div className="flex gap-5">
+          <Button
+            className="bg-blue-500"
+            leftIcon={<IconPlus />}
+            onClick={() => selecionar(transacaoVazia)}
+          >Nova transação</Button>
+            <SegmentedControl
+              data={[
+                { label: <IconList />, value: 'lista' },
+                { label: <IconLayoutGrid />, value: 'grade' }
+              ]}
+              onChange={tipo => alterarExibicao(tipo as TipoExibicao)}
+            />
+        </div>
+    </div>
+  ) 
+}
+
+  function renderizarTransacoes() {
+    const props = { transacoes, selecionarTransacao: selecionar }
+      return tipoExibicao === 'lista' 
+        ? <Lista {...props} />
+        : <Grade {...props} />
+  }
+
   return(
     <Pagina>
-      <Header/>
-        <Button
-          className="bg-blue-500"
-          leftIcon={<IconPlus />}
-          onClick={() => selecionar(transacaoVazia)}
-          >Nova transação</Button>
+      <Header/>      
       <Conteudo className="gap-5">
+      {renderizarControles()}
         {transacao ? (
           <Formulario
             transacao={transacao}
@@ -32,7 +62,7 @@ export default function Financas() {
             excluir={excluir}
           />
         ) : transacoes.length > 0 ? (
-          <Lista transacoes={transacoes} selecionarTransacao={selecionar}/>
+          renderizarTransacoes()
         ) : (
           <NaoEncontrado>
             Nenhuma transação encontrada
